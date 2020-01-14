910672e72427eb3bb37680462c71a0545a0fc5bf
divyabharathik <divya.bhaarathy@gmail.com>
2020-01-14 17:02:09 +0530

    search bar

--- troubleshooting/troubleshootingtool/src/main/resources/application/src/app/app.module.ts
+++ troubleshooting/troubleshootingtool/src/main/resources/application/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
