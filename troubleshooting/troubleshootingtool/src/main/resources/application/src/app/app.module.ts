f5937d49d53ba29d72eec737b261f965dae29128
divyabharathik <divya.bhaarathy@gmail.com>
2020-01-20 10:56:40 +0530

    search bar addition

--- troubleshooting/troubleshootingtool/src/main/resources/application/src/app/app.module.ts
+++ troubleshooting/troubleshootingtool/src/main/resources/application/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -12,8 +11,7 @@ import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
