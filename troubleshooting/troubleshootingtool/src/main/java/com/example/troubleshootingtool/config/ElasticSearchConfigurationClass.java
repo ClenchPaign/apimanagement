c39c4ef4070cca35d950fb1146dcae6411b7591b
divyabharathik <divya.bhaarathy@gmail.com>
2020-01-21 14:27:33 +0530

    Update ElasticSearchConfigurationClass.java

--- troubleshooting/troubleshootingtool/src/main/java/com/example/troubleshootingtool/config/ElasticSearchConfigurationClass.java
+++ troubleshooting/troubleshootingtool/src/main/java/com/example/troubleshootingtool/config/ElasticSearchConfigurationClass.java
@@ -58,8 +58,8 @@ public class ElasticSearchConfigurationClass extends AbstractFactoryBean<RestHig
         try {
             restHighLevelClient = new RestHighLevelClient(
                     RestClient.builder(
-                            new HttpHost("192.168.99.1", 9200, "http"),
-                            new HttpHost("192.168.99.1", 9201, "http"))
+                            new HttpHost("10.60.37.26", 9200, "http"),
+                            new HttpHost("10.60.37.26", 9201, "http"))
                             .setRequestConfigCallback(
                                     new RestClientBuilder.RequestConfigCallback() {
                                         @Override
