c7cfa04610cd43a283b854df6979e160864a50e9
divyabharathik <divya.bhaarathy@gmail.com>
2020-03-19 15:17:02 +0530

    Port changed to 4343

--- troubleshooting/troubleshootingtool/src/main/resources/application/src/app/listing.service.ts
+++ troubleshooting/troubleshootingtool/src/main/resources/application/src/app/listing.service.ts
@@ -26,7 +26,7 @@ export class ListingService {
   constructor(private http: HttpClient) {
   }
 
-  baseUrl = 'http://localhost:4444';
+  baseUrl = 'http://localhost:4343';
   getAllQuestions() {
     return this.http.get(this.baseUrl + '/qnas', httpOptions);
   }
